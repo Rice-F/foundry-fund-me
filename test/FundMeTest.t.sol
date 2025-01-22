225851896f78a1a46dcdeaf2a80e7147c9c4a172
19951680900 <2604211051@qq.com>
2025-01-22 10:17:32 +0800

    snapsshot

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -13,6 +13,9 @@ contract FundMeTest is Test {
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
 
+    // gas
+    // uint256 constant GAS_PRICE = 1;
+
     // 钩子函数，在每个测试函数之前自动执行
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
@@ -75,8 +78,15 @@ contract FundMeTest is Test {
         uint256 startingOwnerBalance = fundMe.getOwner().balance;
         uint256 startingFundMeBalance = address(fundMe).balance;
 
+        // vm.txGasPrice(GAS_PRICE); // 设置每单位所需支付的以太币数量
+        // uint256 gasStart = gasleft();
+
         vm.prank(fundMe.getOwner());
         fundMe.withdraw();
+
+        // uint256 gasEnd = gasleft();
+        // uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        // console.log(gasUsed);
 
         uint256 endingOwnerBalance = fundMe.getOwner().balance;
         uint256 endingFundMeBalance = address(fundMe).balance;
@@ -104,6 +114,31 @@ contract FundMeTest is Test {
 
         vm.startPrank(fundMe.getOwner());
         fundMe.withdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
+
+    function testCheaperWithdrawFormMultipleFunders() public funded {
+        // address()类型转换时，无法直接从uint256转换为地址，需要从uint160转换
+        uint160 numberOfFunders = 2;
+        uint160 startingFunderIndex = 1; // 无法从0开始，address(0)是无效地址
+
+        // 设置多个账户分别向合约转账
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            hoax(address(1), SEND_VALUE); // 模拟一个虚拟账户进行交易，并设置一个prank
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
         vm.stopPrank();
 
         assert(address(fundMe).balance == 0);
